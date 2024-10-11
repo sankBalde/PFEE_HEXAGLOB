96a5533cd45193669eccb7ab12cce172d0879c0e
Abdoulaye Balde <abdoulaye.balde@epita.fr>
2024-10-11 05:25:18 +0200

    up pose visualisation

--- filter_data_AND_convert_in_pose/pose_visualisation.py
+++ filter_data_AND_convert_in_pose/pose_visualisation.py
@@ -1,8 +1,8 @@
 from pose_format import Pose
 from pose_format.pose_visualizer import PoseVisualizer
 
-"""
-with open("noisy.pose", "rb") as f:
+
+with open("achat.pose", "rb") as f:
     pose = Pose.read(f.read())
 
 for c in pose.header.components:
@@ -10,15 +10,4 @@ for c in pose.header.components:
     print("---------------------------------------------------------------")
 v = PoseVisualizer(pose)
 
-v.save_video("bon_noisy3.mp4", v.draw())
-"""
-
-with open("accompagner.pose", "rb") as f:
-    pose = Pose.read(f.read())
-
-for c in pose.header.components:
-    print(c.name, c.points)
-    print("---------------------------------------------------------------")
-v = PoseVisualizer(pose)
-
-v.save_video("accompagner3.mp4", v.draw())
+v.save_video("achat.mp4", v.draw())
